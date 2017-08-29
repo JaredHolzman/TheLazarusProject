4cab65cda897ae6a870f38933c7d4543624be9b6
Jared Holzman <jared@holzman.io>
2017-08-29 01:07:38 -0400

    Start to add symlinking functionality.

--- config.py
+++ config.py
@@ -2,15 +2,17 @@
 import os
 import re
 
+# Directives to match on
+# file_extension: (target_path, prepend_to_file, append_to_file)
 directives = {
-    'symh': '$HOME',
-    'symc': '$HOME/.config'
+    'symh': ('$HOME', '.', ''),
+    'symc': ('$HOME/.config', '', '')
 }
 
 def build_directive_dict():
     return {key: [] for key in directives}
 
-def walk_layers():
+def find_directive_matches():
     exclude = {'.git'}
     # Dictionary to add files/dirs to by their respecitive directives
     matches = build_directive_dict()
@@ -23,11 +25,24 @@ def walk_layers():
         for key in matches:
             matches[key] += [d for d in dirs if re.fullmatch(".*\.{0}".format(key), d) is not None]
             matches[key] += [f for f in files if re.fullmatch(".*\.{0}".format(key), f) is not None]
-        
-    print(matches)
+    return matches
+
+# Meat and potatoes of the operation, here we are symlinking all our configs
+# based on their directive
+def symlink_directives(directives_files):
+    for direc in directives:
+        (path, prepend, append) = directives[direc]
+        base_path = os.path.expandvars(path)
+        for f in directives_files[direc]:
+            file_name = re.split("\.{0}".format(direc),
+                                 "{0}{1}{2}".format(prepend,f,append))[0]
+            target_path = os.path.join(base_path, file_name)
+            print(target_path)
+
 
 def main():
-    walk_layers()
+    matches = find_directive_matches()
+    symlink_directives(matches)
 
 
 if __name__ == "__main__":
