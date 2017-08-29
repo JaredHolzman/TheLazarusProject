d18ca2a4de4a4fc807b1144f023fae1d75890bf6
Jared Holzman <jared@holzman.io>
2017-08-29 18:12:02 -0400

    Refactoring.

--- config.py
+++ config.py
@@ -30,23 +30,29 @@ def find_directive_matches():
             matches[key] += [os.path.join(root, f) for f in files if re.fullmatch(".*\.{0}".format(key), f) is not None]
     return matches
 
-def rm (path):
-    if os.path.isdir(path):
-        print ("Removing directory {0}".format(path))
-        os.rmdir(path)
-    elif os.path.isfile(path):
-        print ("Removing file {0}".format(path))
-        os.remove(path)
+def remove (file_path):
+    if os.file_path.isdir(file_path):
+        print ("Removing directory {0}".format(file_path))
+        os.rmdir(file_path)
+    elif os.file_path.isfile(file_path):
+        print ("Removing file {0}".format(file_path))
+        os.remove(file_path)
     else:
-        print ("Path {0} doesn't exist!".format(path))
+        print ("File {0} doesn't exist!".format(file_path))
+
+def backup (file_path):
+    file_name = os.path.split(path)[1]
+    backup_path = os.path.join(CWD, 'backups', "{0}_backup".format(file_name))
+    print ("Backing up {0} -> {1}".format(file_path, backup_path))
+    shutil.move(file_path, backup_path)
 
 # Meat and potatoes of the operation, here we are symlinking all our configs
 # based on their directive
 def symlink_directives(directives_files):
+    skip_all, remove_all, backup_all = False, False, False
     for direc in directives:
         (path, prepend, append) = directives[direc]
         base_path = os.path.expandvars(path)
-        skip_all, overwrite_all, backup_all = False, False, False
         for f in directives_files[direc]:
             file_name = os.path.split(f)[1]
             target_file_name = re.split(
@@ -58,42 +64,37 @@ def symlink_directives(directives_files):
             if (os.path.exists(target_path)):
                 if skip_all:
                     action = 's'
-                elif overwrite_all:
+                elif remove_all:
                     action = 'o'
                 elif backup_all:
                     action = 'b'
                 else:
                     action = input(
                         "Target {0} already exists, what do you want to do?\n"
-                        + "[s]kip, [S]kip all, [o]verwrite, [O]verwrite all, "
-                        + "[b]ackup, [B]ackup all: ".format(target_path))
+                        .format(target_path)
+                        + "[s]kip, [S]kip all, [r]emove, [R]emove all, "
+                        + "[b]ackup, [B]ackup all: ")
 
-                if action == 's':
+                if action == 'S':
+                    skip_all = True
                     print ('Skipping {0}'.format(target_path))
                     continue
-                elif action == 'S':
-                    skip_all == True
+                elif action == 's':
                     print ('Skipping {0}'.format(target_path))
                     continue
-                elif action == 'o':
-                    rm(target_path)
-                elif action == 'O':
-                    overwrite_all == True
-                    rm(target_path)
-                elif action == 'b':
-                    dest = os.path.join(CWD, 'backups',
-                                        "{0}_backup".format(file_name))
-                    print ("Backing up {0} -> {1}".format(target_path, dest))
-                    shutil.move(target_path, dest)
+                elif action == 'R':
+                    remove_all = True
+                    remove(target_path)
+                elif action == 'r':
+                    remove(target_path)
                 elif action == 'B':
-                    backup_all == True
-                    dest = os.path.join(CWD, 'backups',
-                                        "{0}_backup".format(file_name))
-                    print ("Backing up {0} -> {1}".format(target_path, dest))
-                    shutil.move(target_path, dest)
+                    backup_all = True
+                    backup(target_path)
+                else:
+                    backup(target_path)
 
-                print ("Symlinking {1} -> {0}".format(f, target_path))
-                os.symlink(f, target_path) 
+            print ("Symlinking {1} -> {0}".format(f, target_path))
+            os.symlink(f, target_path) 
 
 def main():
     matches = find_directive_matches()
